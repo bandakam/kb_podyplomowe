2a453bc1f60e3952a4090aa0da716b1c166986f3
banda <banda@wszib.edu.pl>
2019-11-23 16:06:43 +0100

    Prog1

---
+++ src/Prog1.java
@@ -0,0 +1,10 @@
+public class Prog1 {
+    public static void main(String[] args) {
+        System.out.println("Kamil");
+
+        int a = 2;
+        int b = 3;
+        System.out.println("Suma a + b = " + (a + b));
+
+    }
+}
